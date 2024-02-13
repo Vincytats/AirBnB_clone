d9b7a4444e84196ab75778f7345a25710571555e
Vincytats <vincenttats@gmail.com>
2024-02-13 08:04:34 -0800

    Commit

--- console.py
+++ console.py
@@ -5,15 +5,6 @@
 import cmd
 import models
 import re
-from shlex import split
-from models import storage
-from models.base_model import BaseModel
-from models.user import User
-from models.state import State
-from models.city import City
-from models.place import Place
-from models.amenity import Amenity
-from models.review import Review
 
 class HBNBCommand(cmd.Cmd):
     """Command interpreter class"""
